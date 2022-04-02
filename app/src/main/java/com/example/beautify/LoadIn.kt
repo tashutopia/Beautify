7d1c8ade49cacaa692fc3f92956df00461f915da
Chidobem Udejiofor <cudejiofor6@gatech.edu>
2022-04-02 06:40:00 -0400

    Real Start

---
+++ app/src/main/java/com/example/beautify/LoadIn.kt
@@ -0,0 +1,19 @@
+package com.example.beautify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.load_main)
+
+        //Run mogo recovery code until success
+
+        val intent = Intent(this, MapsActivity::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+}
