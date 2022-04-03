dce2bb9517cc10203b1cc441f8e7fabac4d90719
Chidobem Udejiofor <cudejiofor6@gatech.edu>
2022-04-03 01:15:27 -0400

    FT1-MAP10 Stable

--- app/src/main/java/com/example/beautify/MapsActivity.kt
+++ app/src/main/java/com/example/beautify/MapsActivity.kt
@@ -1,62 +1,30 @@
 package com.example.beautify
 
+//import com.google.maps.example.R
 
-import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import android.location.LocationRequest
-import android.os.Build
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-
+class MapsActivity : AppCompatActivity(), OnMyLocationButtonClickListener,
+    OnMyLocationClickListener, OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()
-        ) { permissions ->
-            when {
-                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    // Precise location access granted.
-                }
-                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    // Only approximate location access granted.
-                } else -> {
-                // No location access granted.
-            }
-            }
-        }
-
-// ...
-
-// Before you perform the actual permission request, check whether your app
-// already has the permissions, and whether your app needs to show a permission
-// rationale dialog. For more details, see Request permissions.
-        locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION))
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-
-
 
         val fab: View = findViewById(R.id.camButton)
         fab.setOnClickListener {
@@ -77,11 +45,6 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-
-
-
     }
 
 
@@ -94,28 +57,44 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.getUiSettings().setRotateGesturesEnabled(false);
+        mMap.uiSettings.isRotateGesturesEnabled = false;
         mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-
-
 
         mMap.setMinZoomPreference(10.0f)
         mMap.setMaxZoomPreference(16.0f)
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        addMarker("GaTech",33.775778305161886, -84.39633864568813)
 
+        mMap.isMyLocationEnabled = true
+        mMap.setOnMyLocationButtonClickListener(this)
+        mMap.setOnMyLocationClickListener(this)
     }
-    fun createLocationRequest() {
-        val locationRequest = com.google.android.gms.location.LocationRequest.create()?.apply {
-            interval = 10000
-            fastestInterval = 5000
-            priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Current location:\n$location", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT)
+            .show()
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false
+    }
+
+    fun addMarker(name:String, lat:Double,log:Double) {
+        val ref = LatLng(lat, log)
+        mMap.addMarker(MarkerOptions()
+            .position(ref)
+            .title("Marker in $name"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ref))
     }
 }
+
+
+
+
+
